8c86e5743a22106f23ea638537444e86c9cd8412
Nikita-stud <nikita151998de@gmail.com>
2026-03-15 15:26:51 +0700

    I have added a schemas folder with zod schemas,as well as created a form in contact page with success and error messages. The success message disappears after 5s. I have added 3 libraries. React Hook Form, Zod and Resolve. They all work together in the form validation process

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,7 +1,135 @@
+import { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { contactFormSchema } from '../schemas/contactFormSchema.ts';
+
 const ContactPage = () => {
+  const [isSubmitted, setIsSubmitted] = useState(false);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting, isValid },
+    reset,
+  } = useForm({
+    resolver: zodResolver(contactFormSchema),
+    defaultValues: {
+      fullName: '',
+      subject: '',
+      email: '',
+      message: '',
+    },
+    mode: 'onBlurs',
+  });
+
+  const onSubmit = (data) => {
+    console.log('Form data:', data);
+    reset();
+    setIsSubmitted(true);
+    setTimeout(() => setIsSubmitted(false), 5000);
+  };
+
+  const onError = (errors, e) => {
+    const firstErrorField = Object.keys(errors)[0];
+    if (firstErrorField) {
+      const fieldElement = document.getElementsByName(firstErrorField)[0];
+      if (fieldElement) {
+        fieldElement.focus();
+      }
+    }
+  };
+
   return (
     <>
-      <h1>Contact Page</h1>
+      <h1>Contact Us</h1>
+      {isSubmitted ? (
+        <div className="alert alert-success" role="alert">
+          <p>Form successfully submitted</p>
+        </div>
+      ) : (
+        ''
+      )}
+      <form onSubmit={handleSubmit(onSubmit, onError)}>
+        <div>
+          <label htmlFor="fullName">Full name:</label>
+          <input
+            id="fullName"
+            type="text"
+            {...register('fullName')}
+            aria-invalid={errors.fullName ? 'true' : 'false'}
+            aria-describedby="fullNameError"
+          />
+          {errors.fullName && (
+            <p
+              id="fullNameError"
+              role="alert"
+              style={{ color: 'red', fontSize: '0.9em' }}
+            >
+              {errors.fullName.message}
+            </p>
+          )}
+        </div>
+        <div>
+          <label htmlFor="subject">Subject:</label>
+          <input
+            id="subject"
+            type="text"
+            {...register('subject')}
+            aria-invalid={errors.subject ? 'true' : 'false'}
+            aria-describedby="subjectError"
+          />
+          {errors.subject && (
+            <p
+              id="subjectError"
+              role="alert"
+              style={{ color: 'red', fontSize: '0.9em' }}
+            >
+              {errors.subject.message}
+            </p>
+          )}
+        </div>
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input
+            id="email"
+            type="email"
+            {...register('email')}
+            aria-invalid={errors.email ? 'true' : 'false'}
+            aria-describedby="emailError"
+          />
+          {errors.email && (
+            <p
+              id="emailError"
+              role="alert"
+              style={{ color: 'red', fontSize: '0.9em' }}
+            >
+              {errors.email.message}
+            </p>
+          )}
+        </div>
+        <div>
+          <label htmlFor="message">Message:</label>
+          <textarea
+            id="message"
+            {...register('message')}
+            aria-invalid={errors.message ? 'true' : 'false'}
+            aria-describedby="messageError"
+          />
+          {errors.message && (
+            <p
+              id="messageError"
+              role="alert"
+              style={{ color: 'red', fontSize: '0.9em' }}
+            >
+              {errors.message.message}
+            </p>
+          )}
+        </div>
+        <button type="submit" disabled={isSubmitting || !isValid}>
+          {' '}
+          {isSubmitting ? 'Sending in...' : 'Contact Us'}
+        </button>
+      </form>
     </>
   );
 };
---
+++ src/schemas/contactFormSchema.ts
@@ -0,0 +1,42 @@
+import { z } from 'zod';
+
+const NORWEGIAN_EMAIL_REGEX =
+  /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+const STRING_REGEX = /^[a-zA-ZæøåÆØÅ .'-]+$/;
+
+export const contactFormSchema = z.object({
+  fullName: z
+    .string()
+    .trim()
+    .min(3, { message: 'Full Name must be at least 3 characters.' })
+    .max(50, { message: 'Full Name can not be longer than 50 characters' })
+    .regex(STRING_REGEX, {
+      message: 'Full Name must contain allowed characters',
+    }),
+  subject: z
+    .string()
+    .trim()
+    .min(3, { message: 'Subject must be at least 3 characters.' })
+    .max(50, { message: 'Subject can not be longer than 50 characters' })
+    .regex(STRING_REGEX, {
+      message: 'Subject must contain allowed characters',
+    }),
+  email: z
+    .string()
+    .trim()
+    .email('Invalid email address')
+    .min(3, { message: 'Email must be at least 3 characters.' })
+    .max(50, { message: 'Email can not be longer than 50 characters' })
+    .refine((mail) => NORWEGIAN_EMAIL_REGEX.test(mail), {
+      message: 'Email seems to contain a mistake',
+    }),
+  message: z
+    .string()
+    .trim()
+    .min(10, { message: 'Message must be at least 10 characters.' })
+    .max(500, { message: 'Message can not be longer than 500 characters' })
+    .regex(STRING_REGEX, {
+      message: 'Message must contain allowed characters',
+    }),
+});
